2fe852c86d39a69ae3bd866ee349cdf307a37dd4
Boyd Hamilton <boydroberthamilton@gmail.com>
2024-02-28 20:19:37 -0700

    unigrams

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # spellcheck
 
-Quick spelling correction program using Levenshtein distance in Java. Gives mispelled words spelling suggestions of an input phrase based off a [database of 370,101 English words](https://www.kaggle.com/datasets/ruchi798/part-of-speech-tagging/data). Example contains recursive and matrix implementation with respective time complexities of `O(3^(m+n))` and `O(mn)`. Program uses matrix implementation, but recursive is present as a learning excercise.
+Quick spelling correction program using Levenshtein distance and n-grams in Java. Gives mispelled words spelling suggestions of an input phrase based off a [database of 370,101 English words](https://www.kaggle.com/datasets/ruchi798/part-of-speech-tagging/data) as our dictionary, and one of the [333,333 most common words](https://www.kaggle.com/datasets/rtatman/english-word-frequency?resource=download). Algorithm generates guesses of correct spellings based off of matrix distance, then sorts the guesses by popularity using unigrams. Because none of the data uses sentances or words in sequence, we cannot use any order of n-gram higher than one, which renders us unable to use context to provide suggestions.
 
 [Source paper](https://www.irjet.net/archives/V8/i9/IRJET-V8I9316.pdf)
+[Source paper](https://fileadmin.cs.lth.se/cs/education/EDA171/Reports/2009/david.pdf)
 
 ![Levenshtein Formula](github-resources/levenshtein-formula.png "Levenshtein Formula")
--- spellcheck.java
+++ spellcheck.java
@@ -1,39 +1,14 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
+
+
+
+// boyd hamilton 2024
+
 
 public class spellcheck{
 
-    
-
-    static int levenshteinRecursive(String given, String check, int m, int n){ // time complexity: O(3^(m+n))
-    /*  three edit moves are:
-            - removal
-            - addition
-            - substitution
-        each of these yields an edit distance of 1
-    */
-        if(m==0){
-            return n;
-        }else if(n==0){
-            return m;
-        }
-        char[] givencarr = given.toCharArray();
-        char[] checkcarr = check.toCharArray();
-        
-        if(givencarr[m-1] == checkcarr[n-1]){
-            return levenshteinRecursive(given, check, m-1, n-1);
-        }
-        
-        return 1 + Math.min(
-        levenshteinRecursive(given, check, m, n-1),Math.min( // insertion
-        levenshteinRecursive(given, check, m-1, n), // removal
-        levenshteinRecursive(given, check, m-1, n-1))); // substitution
-    }
     static int levenshteinMatrix(String given, String check){
         int m = given.length();
         int n = check.length();
@@ -62,23 +37,27 @@ public class spellcheck{
         return matrix[m][n];
     }
 
-
-    public static String[] suggestions(String phrase){
-        String fileName = "data/words_pos.csv";
-        List<List<String>> rows = new ArrayList<>();
-
-        try{
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                rows.add(Arrays.asList(values));
-            }
-            br.close();
-        }catch(IOException exception){
-            exception.printStackTrace();
+    static void parseData(List<List<String>> rows, List<String> rank) throws IOException{ // TODO: thread
+        String wordsFileName = "data/words_pos.csv";
+        String unigramFileName = "data/unigram_freq.csv";
+        BufferedReader br = new BufferedReader(new FileReader(wordsFileName));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] values = line.split(",");
+            rows.add(Arrays.asList(values));
         }
+        br.close();
+        br = new BufferedReader(new FileReader(unigramFileName));
+        while ((line = br.readLine()) != null) {
+            String v = line.split(",")[0];
+            rank.add(v);
+        }
+        br.close();
+    }
 
+
+    public static String[] levenshteinSuggestions(String phrase,List<List<String>> rows){
+        
         String[] words = new String[rows.size()];
         for(int i=0; i<rows.size(); i++){
             List<String> row = rows.get(i);
@@ -88,8 +67,8 @@ public class spellcheck{
         List<String> possibilities = new ArrayList<>();
         if (!Arrays.asList(words).contains(phrase)){
             int range = 2; // current best
-            for(String word : words){
-                int d = levenshteinMatrix(phrase, word);
+            for(String word : words){ // could stream
+                int d = levenshteinMatrix(phrase, word); // ngl instead of ints i could be returning the values as bytes. edit distance is not 32 bits
                 if(d<range){
                     possibilities.clear();
                     range = d;
@@ -99,27 +78,75 @@ public class spellcheck{
                 }
             }
         }else{
-            return new String[] {"Correct"};
+            return new String[] {"c"}; // correctly spelled (in our dictionary)
         }
-        return possibilities.toArray(new String[0]);
+
+        // if no possibilities, we failed to find a word in acceptable edit distance + it was not in our dictionary
+        return (possibilities.size()>1) ? possibilities.toArray(new String[0]) : new String[] {"f"}; 
     }
 
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        String[] phrase = scanner.nextLine().split(" ");
-        scanner.close();
+    // with the unigrams we're just sorting the suggestions we got from levenschtein on popularity
+    static String[] unigramSorting(String[] guesses, List<String> rank){
+        List<String> l_guesses = Arrays.asList(guesses);
 
+        Comparator<String> best = new Comparator<String>() {
+            @Override
+            public int compare(String a, String b){
+                boolean air = rank.contains(a); // a/b in rank
+                boolean bir = rank.contains(b);
+
+                if (air && bir){
+                    return Float.compare(rank.indexOf(a), rank.indexOf(b));
+
+                // if in freq list at all, instantly more valid than if not
+                }else if(air){
+                    return -1; // a comes before b
+                }else if(bir){
+                    return 1; // b comes before a
+                }else{
+                    return 0; // equal, sort others around it
+                }
+            }
+        };
+        Collections.sort(l_guesses,best);
+
+        return l_guesses.toArray(new String[0]);
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] phrase = br.readLine().split(" ");
+        br.close();
+
+        // parse csvs
+        List<List<String>> rows = new ArrayList<>();
+        List<String> rank = new ArrayList<>();
+        try{ // TODO: thread
+            parseData(rows, rank);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        // out
         for(String s : phrase){
-            String[] corrections = suggestions(s.toLowerCase());
-            if(corrections[0]!="Correct"){
+            String[] guesses = levenshteinSuggestions(s.toLowerCase(), rows);
+            String[] corrections = 
+            (guesses[0].equals("c") || guesses[0].equals("f")) // special cases (correct, failed)
+            ? guesses
+            : unigramSorting(guesses, rank) ;
+           
+            if(corrections[0]!="c" && corrections[0]!="f"){ 
                 System.out.println("\n ("+s+") Did you mean...");
                 for(String maybe : corrections){
                     System.out.print(maybe+" ");
                 }
                 System.out.println("");
             }
-            
         }
-        
+
+        // approximation of memory used to run the program in the jvm
+        long mem =Runtime.getRuntime().totalMemory()- Runtime.getRuntime().freeMemory();
+        mem=(long)((double)mem/Math.pow(10.0,6.0));
+        System.out.println("\n~"+mem+" megabytes");
     }
 }
